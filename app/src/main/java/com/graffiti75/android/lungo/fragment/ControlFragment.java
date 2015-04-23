34c798d370fdf06487b7938f88062be21803b6f1
graffiti75 <graffiti75@gmail.com>
2015-04-23 22:03:49 +0100

    ControlFragment incrementation implemented.

--- app/src/main/java/com/graffiti75/android/lungo/fragment/ControlFragment.java
+++ app/src/main/java/com/graffiti75/android/lungo/fragment/ControlFragment.java
@@ -16,7 +16,7 @@ import com.graffiti75.android.lungo.R;
  * @author Rodrigo Cericatto
  * @since March 25, 2015
  */
-public class ControlFragment extends Fragment {
+public class ControlFragment extends Fragment implements View.OnClickListener {
 
 	//--------------------------------------------------
 	// Attributes
@@ -24,6 +24,7 @@ public class ControlFragment extends Fragment {
 	
 	private TextView mSmokedCigarettesTextView;
     private TextView mSmokingNowTextView;
+    private Integer mCounter = 0;
 
 	//--------------------------------------------------
 	// Constructor
@@ -55,6 +56,7 @@ public class ControlFragment extends Fragment {
     public void setLayout(View view) {
         mSmokedCigarettesTextView = (TextView)view.findViewById(R.id.id_smoked_cigarettes_text_view);
         mSmokingNowTextView = (TextView)view.findViewById(R.id.id_smoking_now_text_view);
+        mSmokingNowTextView.setOnClickListener(this);
     }
 
 	/**
@@ -65,4 +67,57 @@ public class ControlFragment extends Fragment {
         mSmokedCigarettesTextView.setTypeface(gothamBook);
         mSmokingNowTextView.setTypeface(gothamBook);
 	}
+
+    /**
+     * Changes the color of the mSmokingNowTextView.
+     */
+    public void changeColor() {
+        Integer mod = mCounter % 8;
+
+        switch (mod) {
+            case 0:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_green_behavior));
+                break;
+            case 1:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_yellow_light_behavior));
+                break;
+            case 2:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_yellow_behavior));
+                break;
+            case 3:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_orange_light_behavior));
+                break;
+            case 4:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_orange_behavior));
+                break;
+            case 5:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_carrot_behavior));
+                break;
+            case 6:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_red_dark_behavior));
+                break;
+            case 7:
+                mSmokingNowTextView.setBackground(getActivity().getDrawable(R.drawable.rounded_button_red_behavior));
+                break;
+        }
+    }
+
+    //--------------------------------------------------
+    // Listeners
+    //--------------------------------------------------
+
+    @Override
+    public void onClick(View view) {
+        mCounter++;
+
+        // Formats value.
+        String value = mCounter.toString();
+        if (mCounter < 10) {
+            value = "0" + value;
+        }
+
+        // Change its value.
+        mSmokedCigarettesTextView.setText(value);
+        changeColor();
+    }
 }
